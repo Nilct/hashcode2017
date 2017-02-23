794de5f060c859923074d0eb21363bd33591b73a
Tofull <loic.messal@orange.fr>
2017-02-23 20:02:41 +0100

    implementation Endpoint

--- src/EndPoint.java
+++ src/EndPoint.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
  * Created by Camille on 23/02/2017.
  */
 public class EndPoint {
-    ArrayList<EndPoint> listOfEp;
-    //ArrayList<>
+    int id;
+    int latencyDatacenter;
+    int[] latency;
+
+
+    EndPoint(int id, int latencyDatacenter, int nbTotalCache){
+      this.id = id;
+      this.latencyDatacenter = latencyDatacenter;
+
+      latency = new int[nbTotalCache];
+      for (int c = 0 ; c < latency.length; c++) {
+        latency[c] = -1;
+      }
+    }
+
+    public void changeLatency(int fromCache, int latencyValue){
+      latency[fromCache] = latencyValue;
+    }
 }
