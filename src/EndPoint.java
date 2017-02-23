20f3e25df710fb6b07a9b03da607a43868a8148c
Tofull <loic.messal@orange.fr>
2017-02-23 20:20:47 +0100

    read endpoint lines

--- src/EndPoint.java
+++ src/EndPoint.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
  * Created by Camille on 23/02/2017.
  */
 public class EndPoint {
+    int DEFAULT = -1;
     int id;
     int latencyDatacenter;
     int[] latency;
@@ -15,11 +16,21 @@ public class EndPoint {
 
       latency = new int[nbTotalCache];
       for (int c = 0 ; c < latency.length; c++) {
-        latency[c] = -1;
+        latency[c] = DEFAULT;
       }
     }
 
     public void changeLatency(int fromCache, int latencyValue){
       latency[fromCache] = latencyValue;
+    }
+
+    public int countNotDefault(){
+      int result = 0;
+      for (int c = 0; c < latency.length; c++){
+        if (latency[c]==DEFAULT) {
+            result++;
+        }
+      }
+      return result;
     }
 }
